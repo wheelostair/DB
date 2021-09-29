18e49833f0eccf888d3baecd37619caa3a707b1e
Alastair Cox <5coxa14@solent.ac.uk>
2021-09-29 15:58:48 +0100

    first week

---
+++ index.js
@@ -0,0 +1,38 @@
+const http = require("http");
+const server = http.createServer();
+const port = "8080";
+const { MongoClient } = require("mongodb");
+
+const url = "mongodb+srv://test:test@cluster0.zbahk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const database = "dbwork"
+
+const client = new MongoClient(url, { useNewUrlParser: true });
+
+const students = [
+    {
+        name: { first: "joe", last: "appleton" },
+        dob: new Date("August 12, 1982"),
+    },
+    {
+        name: { first: "bill", last: "smith" },
+        dob: new Date("August 22, 1982"),
+    },
+];
+
+server.on("request", async (req, res) => {
+
+    try {
+        await client.connect();
+        const db = client.db(database);
+        const collection = db.collection("students")
+        await collection.insertMany(students);
+        res.end("Request Ended");
+
+    } catch (error) {
+        console.log(`could not update ${error}`);
+    }
+});
+
+
+
+server.listen(8080);
